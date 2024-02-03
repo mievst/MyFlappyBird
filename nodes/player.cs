2930f86010ebc202be94588ddf2dc17015da3da0
Evstifeev Mikhail <mievstifieiev1999@gmail.com>
2024-02-03 22:08:59 +0300

    поправил управление птичкой

--- nodes/player.cs
+++ nodes/player.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 public partial class player : CharacterBody2D
 {
 	public const float Gravity = 50f;
-	public const float Jump = -1100f;
+	public const float Jump = -1000f;
 	public bool Started = false;
 	public bool IsDead = false;
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -19,6 +19,7 @@ public partial class player : CharacterBody2D
 		if (!Velocity.IsZeroApprox() || Started)
 		{
 			velocity.Y += Gravity;
+			if (velocity.Y > 1000) velocity.Y = 1000;
 			if (velocity.Y > 0)
 			{
 				var sprite = (AnimatedSprite2D)FindChild("AnimatedSprite2D");
@@ -30,7 +31,7 @@ public partial class player : CharacterBody2D
 		if (Input.IsActionJustPressed("ui_accept") && !IsDead)
 		{
 			Started = true;
-			if (Position.Y < 500) 
+			/* if (Position.Y < 500)
 			{
 				velocity.Y += Jump / 500 - Position.Y;
 			}
@@ -38,20 +39,21 @@ public partial class player : CharacterBody2D
 			{
 				velocity.Y += Jump;
 
-			}
+			} */
+			velocity.Y = Jump;
 			var sprite = (AnimatedSprite2D)FindChild("AnimatedSprite2D");
 			sprite.Animation = "Up";
 		}
-			
+
 
 		Velocity = velocity;
 		bool colided = MoveAndSlide();
 		if (Position.Y < 5)
 		{
-			Position = new Vector2 
-			{ 
+			Position = new Vector2
+			{
 				X = Position.X,
-				Y = 5 
+				Y = 5
 			};
 		}
 		if (colided)
